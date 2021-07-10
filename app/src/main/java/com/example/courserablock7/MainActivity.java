72c173736775189f822158803fe3d6a52c691da6
naman2807 <namanagarwaldbs438@gmail.com>
2021-07-10 07:34:16 +0530

    Declared NumberPicker variable

--- app/src/main/java/com/example/courserablock7/MainActivity.java
+++ app/src/main/java/com/example/courserablock7/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.courserablock7;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.NumberPicker;
 
 public class MainActivity extends AppCompatActivity {
+    NumberPicker possibilities;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
